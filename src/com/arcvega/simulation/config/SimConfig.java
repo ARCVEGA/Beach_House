1249a3326ae65e8948917341399b6473d3d76945
confiscar <24923564+confiscar@users.noreply.github.com>
2020-09-18 18:31:11 +0200

    Added the settings class to concatenate the simulation parameters

---
+++ src/com/arcvega/simulation/config/SimConfig.java
@@ -0,0 +1,15 @@
+package com.arcvega.simulation.config;
+
+public class SimConfig {
+
+  public static final int SIM_DISCRETIZATION = 1;
+  public static final int SIM_WIDTH = 200;
+  public static final int SIM_HEIGHT = 200;
+
+  public static final int CASEY_AMOUNT = 20;
+  public static final double CASEY_THRESHOLD_DISTANCE = 10;
+
+  public static final double JIM_THRESHOLD_DISTANCE = 5;
+
+  public static final int MATT_AMOUNT = 20;
+}
