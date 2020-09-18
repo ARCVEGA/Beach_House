9c13ff34413eee1b71bd92ccdd13fa369e9673d7
confiscar <24923564+confiscar@users.noreply.github.com>
2020-09-18 17:35:28 +0200

    little more docs

--- src/com/arcvega/simulation/agents/Agent.java
+++ src/com/arcvega/simulation/agents/Agent.java
@@ -6,10 +6,12 @@ import sim.util.Double2D;
 
 public abstract class Agent implements Steppable {
 
+    /**Function that provides a simple wrapper around moving to a location for readability.*/
     void walkTowards(Simulation sim, Double2D dest){
         sim.space.setObjectLocation(this, dest);
     }
 
+    /**Function that just moves agent to random walk.*/
     void randomWalk(Simulation sim){
         Double2D location = sim.space.getObjectLocation(this);
         Double2D newLocation = new Double2D(location.getX() + (sim.random.nextDouble() - 0.5), location.getY() + (sim.random.nextDouble() - 0.5));
